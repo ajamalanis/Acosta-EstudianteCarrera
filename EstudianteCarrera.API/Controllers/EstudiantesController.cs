f1998a77a1d80bb6d76a7eebdc770cc75c628774
Alanis Acosta <alanissjusstine@gmail.com>
2023-05-25 17:14:05 -0600

    Nuevo commit

---
+++ EstudianteCarrera.API/Controllers/EstudiantesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EstudianteCarrera.Models;
+
+namespace EstudianteCarrera.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiantesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EstudiantesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Estudiantes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Estudiante>>> GetEstudiante()
+        {
+          if (_context.Estudiante == null)
+          {
+              return NotFound();
+          }
+            return await _context.Estudiante.
+                Include(e => e.Carrera).
+                ToListAsync();
+        }
+
+        // GET: api/Estudiantes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Estudiante>> GetEstudiante(int id)
+        {
+          if (_context.Estudiante == null)
+          {
+              return NotFound();
+          }
+            var estudiante = _context.Estudiante.
+                Include(e => e.Carrera).
+                First(e => e.EstudianteuId == id);
+
+
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            return estudiante;
+        }
+
+        // PUT: api/Estudiantes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEstudiante(int id, Estudiante estudiante)
+        {
+            if (id != estudiante.EstudianteuId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(estudiante).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EstudianteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Estudiantes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Estudiante>> PostEstudiante(Estudiante estudiante)
+        {
+          if (_context.Estudiante == null)
+          {
+              return Problem("Entity set 'DataContext.Estudiante'  is null.");
+          }
+            _context.Estudiante.Add(estudiante);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEstudiante", new { id = estudiante.EstudianteuId }, estudiante);
+        }
+
+        // DELETE: api/Estudiantes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEstudiante(int id)
+        {
+            if (_context.Estudiante == null)
+            {
+                return NotFound();
+            }
+            var estudiante = await _context.Estudiante.FindAsync(id);
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            _context.Estudiante.Remove(estudiante);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EstudianteExists(int id)
+        {
+            return (_context.Estudiante?.Any(e => e.EstudianteuId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ EstudianteCarrera.API/Migrations/20230525172443_v01.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudianteCarrera.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrera",
+                columns: table => new
+                {
+                    CarreraId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    NumEstudiantes = table.Column<int>(type: "INTEGER", nullable: false),
+                    Niveles = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrera", x => x.CarreraId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    EstudianteuId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: false),
+                    CarreraId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.EstudianteuId);
+                    table.ForeignKey(
+                        name: "FK_Estudiante_Carrera_CarreraId",
+                        column: x => x.CarreraId,
+                        principalTable: "Carrera",
+                        principalColumn: "CarreraId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estudiante_CarreraId",
+                table: "Estudiante",
+                column: "CarreraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+
+            migrationBuilder.DropTable(
+                name: "Carrera");
+        }
+    }
+}
