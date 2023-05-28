e7cef9c83626434bc743c699bafc61a141b25025
Alanis Acosta <alanissjusstine@gmail.com>
2023-05-27 18:50:36 -0600

    Committtt

---
+++ EstudianteCarrera.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstudianteCarrera.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
